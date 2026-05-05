528d9474f6eb27af879cb6c33921c5b5816f1dc4
GinoL221 <ginolencina@gmail.com>
2026-05-05 15:47:13 -0300

    fix(test): remove addFilters=false from AppointmentControllerTest to fix 401 with @WithMockUser

--- backend/src/test/java/com/dh/dentalClinicMVC/controller/AppointmentControllerTest.java
+++ backend/src/test/java/com/dh/dentalClinicMVC/controller/AppointmentControllerTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
 @WithMockUser(roles = "ADMIN")
 class AppointmentControllerTest {
 
